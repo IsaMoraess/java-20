64c8fee55a973d852776bec71e3c4c62b4468689
IsaMoraess <135661414+IsaMoraess@users.noreply.github.com>
2024-03-28 11:32:18 -0300

    Add files via upload

---
+++ InformacoesPessoais.java
@@ -0,0 +1,13 @@
+public class InformacoesPessoais {
+
+    public static void main(String[] args) {
+        // Escrevendo informações pessoais
+        System.out.println("Nome: Isabelly Moraes");
+        System.out.println("Endereço: Planaltina DF, Quadra.1 Conjunto.G Casa.49");
+        System.out.println("CEP e Telefone: 73350-107, (61) 994210896");
+        //o método "main" é o ponto de entrada e execução do programa. 
+        //Ele simplesmente usa o método "println" para imprimir as informações pessoais em linhas
+        //separadas, como especificado.
+    }
+}
+
